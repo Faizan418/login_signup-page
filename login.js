8259725addc30ad4f43dff4b73a425b2abc97787
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-11-24 23:07:07 +0500

    update

--- login.js
+++ login.js
@@ -1,18 +1,19 @@
-let prevStudents =localStorage.getItem('students');
+let prevStudents = localStorage.getItem("students");
 let students = prevStudents ? JSON.parse(prevStudents) : [];
 
-function login(){
-    let name = document.getElementById('loginName').value;
-    let password = document.getElementById('loginPassword').value;
+function login() {
+  let name = document.getElementById("loginName").value;
+  let password = document.getElementById("loginPassword").value;
 
-    let found = students.find(student.name === name && student.password === password);
+  let found = students.find(
+    (student) => student.name === name && student.password === password
+  );
 
-    if(found){
-        alert('Login successful');
-        localStorage.setItem('currentUser', name);
-        window.location.href='hero.html';
-
-    }else{
-        alert('Invalid credentials');
-    }
+  if (found) {
+    alert("Login successful");
+    localStorage.setItem("currentUser", name);
+    window.location.href = "hero.html";
+  } else {
+    alert("Invalid credentials");
+  }
 }
--- signup.js
+++ signup.js
@@ -1,20 +1,17 @@
-let prevStudents =localStorage.getItem('students');
+let prevStudents = localStorage.getItem("students");
 let students = prevStudents ? JSON.parse(prevStudents) : [];
 
-function signUp(){
-    let name = document.getElementById('signUpName').value;
-    let password = document.getElementById('signUpPassword').value;
+function signUp() {
+  let name = document.getElementById("signUpName").value;
+  let password = document.getElementById("signUpPassword").value;
 
-    if(name && password){
-        let student = { name, password };
-        students.push(student);
-        localStorage.setItem('students', JSON.stringify(students));
-        alert('Signed Up Successfully');
-        window.location.href='login.html';
-
-
-    }else{
-        alert('Please fill all fields.')
-    }
-    
+  if (name && password) {
+    let student = { name, password };
+    students.push(student);
+    localStorage.setItem("students", JSON.stringify(students));
+    alert("Signed Up Successfully pleace login");
+    window.location.href = "login.html";
+  } else {
+    alert("Please fill all fields.");
+  }
 }
